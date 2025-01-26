3e0b8e1979a87126da161ac2db028ecb601dae6e
puramnagendra2 <puramnagendra2@gmail.com>
2025-01-26 21:22:26 +0530

    Changed

--- majorPro/settings.py
+++ majorPro/settings.py
@@ -78,12 +78,6 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'majorDB',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
     }
 }
 
